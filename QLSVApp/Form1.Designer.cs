e35b91c35040651c2f1a3f69a163f7287f1d4281
nhatnmn2311 <minhnhat.nmn02@gmail.com>
2024-04-26 15:18:22 +0700

    first commit

---
+++ QLSVApp/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace QLSVApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtTimKiem = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnTimKiem = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(87, 25);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(159, 20);
+            this.txtName.TabIndex = 0;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(87, 73);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(159, 20);
+            this.txtPhone.TabIndex = 0;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(87, 122);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(159, 20);
+            this.txtEmail.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 243);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nhập Họ tên/SĐT/Email:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Email";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "SDT";
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(21, 161);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(79, 25);
+            this.btnThem.TabIndex = 2;
+            this.btnThem.Text = "Them";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.Thembttn_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(167, 161);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(79, 25);
+            this.btnSua.TabIndex = 2;
+            this.btnSua.Text = "Sua";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(21, 200);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(79, 25);
+            this.btnXoa.TabIndex = 2;
+            this.btnXoa.Text = "Xoa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(167, 200);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(79, 25);
+            this.btnThoat.TabIndex = 2;
+            this.btnThoat.Text = "Thoat";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(252, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(385, 400);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // txtTimKiem
+            // 
+            this.txtTimKiem.Location = new System.Drawing.Point(20, 259);
+            this.txtTimKiem.Name = "txtTimKiem";
+            this.txtTimKiem.Size = new System.Drawing.Size(226, 20);
+            this.txtTimKiem.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Ho ten";
+            // 
+            // btnTimKiem
+            // 
+            this.btnTimKiem.Location = new System.Drawing.Point(96, 303);
+            this.btnTimKiem.Name = "btnTimKiem";
+            this.btnTimKiem.Size = new System.Drawing.Size(79, 24);
+            this.btnTimKiem.TabIndex = 5;
+            this.btnTimKiem.Text = "Tìm kiếm";
+            this.btnTimKiem.UseVisualStyleBackColor = true;
+            this.btnTimKiem.Click += new System.EventHandler(this.btnTimKiem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 424);
+            this.Controls.Add(this.btnTimKiem);
+            this.Controls.Add(this.txtTimKiem);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtTimKiem;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnTimKiem;
+    }
+}
+
---
+++ QLSVApp/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLSVApp
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        QLSVEntities context = new QLSVEntities();
+        SinhVien sinhVienDuocChon;
+        private void Thembttn_Click(object sender, EventArgs e)
+        {
+            SinhVien sv = new SinhVien()
+            {
+                SV_Name = txtName.Text,
+                SV_Phone = txtPhone.Text,
+                SV_Email = txtEmail.Text
+            };
+            context.SinhViens.Add(sv);
+            context.SaveChanges();
+            MessageBox.Show("Thêm sinh viên thành công!");
+            HienThiDanhSachSinhVien();
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            //int svId = int.Parse(txtId.Text); // Giả sử có một TextBox để nhập ID
+            //SinhVien sv = context.SinhViens.FirstOrDefault(s => s.SV_ID == svId);
+            //if (sv != null)
+            //{
+            //    context.SinhViens.Remove(sv);
+            //    context.SaveChanges();
+            //    MessageBox.Show("Xóa sinh viên thành công!");
+            //    HienThiDanhSachSinhVien();
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Sinh viên không tồn tại!");
+            //}
+            if (sinhVienDuocChon != null)
+            {
+                context.SinhViens.Remove(sinhVienDuocChon);
+                context.SaveChanges();
+                HienThiDanhSachSinhVien(); // Làm mới DataGridView
+                MessageBox.Show("Xóa sinh viên thành công!");
+            }
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            //int svId = int.Parse(txtId.Text); // Giả sử có một TextBox để nhập ID
+            //SinhVien sv = context.SinhViens.FirstOrDefault(s => s.SV_ID == svId);
+            //if (sv != null)
+            //{
+            //    sv.SV_Name = txtName.Text;
+            //    sv.SV_Phone = txtPhone.Text;
+            //    sv.SV_Email = txtEmail.Text;
+            //    context.SaveChanges();
+            //    MessageBox.Show("Cập nhật thông tin sinh viên thành công!");
+            //    HienThiDanhSachSinhVien();
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Sinh viên không tồn tại!");
+            //}
+            if (sinhVienDuocChon != null)
+            {
+                sinhVienDuocChon.SV_Name = txtName.Text;
+                sinhVienDuocChon.SV_Phone = txtPhone.Text;
+                sinhVienDuocChon.SV_Email = txtEmail.Text;
+                context.SaveChanges();
+                HienThiDanhSachSinhVien(); // Làm mới DataGridView
+                MessageBox.Show("Cập nhật thông tin sinh viên thành công!");
+            }
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close(); // Đóng form hiện tại
+        }
+        private void HienThiDanhSachSinhVien()
+        {
+            var danhSachSinhVien = context.SinhViens.ToList();
+            dataGridView1.DataSource = danhSachSinhVien;
+        }
+        
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            HienThiDanhSachSinhVien();
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                int index = dataGridView1.SelectedRows[0].Index;
+                int svId = (int)dataGridView1.Rows[index].Cells["SV_ID"].Value;
+                sinhVienDuocChon = context.SinhViens.FirstOrDefault(s => s.SV_ID == svId);
+                // Đặt giá trị cho các TextBox từ sinhVienDuocChon
+                txtName.Text = sinhVienDuocChon.SV_Name;
+                txtPhone.Text = sinhVienDuocChon.SV_Phone;
+                txtEmail.Text = sinhVienDuocChon.SV_Email;
+            }
+        }
+
+        private void btnTimKiem_Click(object sender, EventArgs e)
+        {
+            string searchTerm = txtTimKiem.Text.ToLower();
+            var ketQuaTimKiem = context.SinhViens.Where(s => s.SV_Name.ToLower().Contains(searchTerm)
+                                                || s.SV_Phone.Contains(searchTerm)
+                                                || s.SV_Email.ToLower().Contains(searchTerm)).ToList();
+            dataGridView1.DataSource = ketQuaTimKiem;
+        }
+    }
+}
